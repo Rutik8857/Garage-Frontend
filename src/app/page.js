178fe3c18aeac2085f072e62a5d0266cdd0b2f95
rutik.tade <rutiktade63@gmail.com>
2026-01-16 13:52:22 +0530

    solving common error

--- src/app/page.js
+++ src/app/page.js
@@ -1,37 +1,26 @@
-'use client'
-import React, {useState} from 'react'
-import Header from './components/Header'
-import Sidebar from './components/Sidebar'
-import MainDash from './main/page'
-
+"use client";
+import React, { useState } from "react";
+import Header from "./components/Header";
+import Sidebar from "./components/Sidebar";
+import MainDash from "./main/page";
 
 const Page = () => {
-
- const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
   const toggleSidebar = () => {
     setIsSidebarOpen((prev) => !prev);
   };
 
-
-  
   return (
     <div>
       <div className="flex bg-gray-300">
+        <Sidebar isOpen={isSidebarOpen} onClose={toggleSidebar} />
 
-      <Sidebar isOpen={isSidebarOpen} onClose={toggleSidebar}/> 
-     
-      
-      <MainDash />
-      
-      
+        <MainDash />
       </div>
       {/* <h1 className="text-xl font-bold text-gray-800 mb-6">Dashboard</h1>   */}
-      
-
-     
     </div>
-  )
-}
+  );
+};
 
-export default Page
+export default Page;
