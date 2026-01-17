0d8ffad7fb75978480f2c11f04733e5c99d16233
rutik.tade <rutiktade63@gmail.com>
2026-01-17 16:00:35 +0530

    solving the error of confirm

--- src/app/components/ClientLayout.js
+++ src/app/components/ClientLayout.js
@@ -1,12 +1,15 @@
 'use client';
 
 import { AlertProvider } from '../context/AlertContext';
+import { ConfirmProvider } from '../context/ConfirmContext';
 import AuthGuard from '../AuthGuard';
 
 export default function ClientLayout({ children }) {
   return (
     <AlertProvider>
-      <AuthGuard>{children}</AuthGuard>
+      <ConfirmProvider>
+        <AuthGuard>{children}</AuthGuard>
+      </ConfirmProvider>
     </AlertProvider>
   );
 }
